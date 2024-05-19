141f5ac0ead2fe513ea5c6638dea1707ba8714ab
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-19 14:15:43 +0600

    responsive about us section

---
+++ note.js
@@ -0,0 +1,6 @@
+/* 
+1. 2 ta sector ar maje dorottoo py-10 
+2. ak e section ar image abong text ar maje dorotto mt-10 (aita hoise sudho matro responsive korar somai a )
+3. responsive korar somai a ak e section ar image abong text ar maje gap-10
+4. spacey-y-6 (aita normally), 
+*/
