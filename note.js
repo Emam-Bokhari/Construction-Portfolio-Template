6de5aa071bdec4b132cf908e017ea33dbb4a5634
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-05-20 22:03:48 +0600

    create service section with title,description and icon

--- note.js
+++ note.js
@@ -3,7 +3,7 @@
 2. ak e section ar image abong text ar maje dorotto mt-10 (aita hoise sudho matro responsive korar somai a )
 3. responsive korar somai a ak e section ar image abong text ar maje gap-10
 4. spacey-y-6 (aita normally), 
-5. space-y-3 md:space-y-4 xl:space-y-6
+5. space-y-3 md:space-y-4
 6. text-[#4D5765] 3rd color,
  font-[titillium],
  font-[archivo],
